a246dd2973e0f5b4e5ffb0fe2a5e8d8f1e53582c
lwzhang <lwzhang@thoughtworks.com>
2015-03-06 18:02:30 +0800

    Initialize project

---
+++ src/main/java/Talks.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lwzhang on 3/6/15.
+ */
+public class Talks {
+
+    private final String talkTitle;
+    private final int talkLength;
+
+    public Talks (String talkTitle, int talkLength) {
+        this.talkTitle = talkTitle;
+        this.talkLength = talkLength;
+    }
+
+    //TODO: Use Regex to identify title and minutes from input file
+}
